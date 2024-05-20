f6e499dc5c3e67fe2217e3312933f5c6939b0ae4
Anatoliy Vikulov <vikulovtolya38@gmail.com>
2024-05-21 00:44:08 +0800

    up

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,29 @@
+// Burger
+const screenWidth = window.screen.width;
+if (screenWidth <= 1023) {
+  const body = document.body;
+  const core = document.querySelector(".core");
+  const burger = document.querySelector(".burger");
+  const close = document.querySelector(".close");
+  const burgerMenu = document.querySelector(".burger__menu");
+  burger.addEventListener("click", () => {
+    core.classList.remove("hide");
+    burgerMenu.classList.remove("hide");
+    body.classList.add("no-scroll");
+  });
+  core.addEventListener("click", (el) => {
+    core.classList.add("hide");
+    burgerMenu.classList.add("hide");
+    body.classList.remove("no-scroll");
+  });
+  close.addEventListener("click", (el) => {
+    core.classList.add("hide");
+    burgerMenu.classList.add("hide");
+    body.classList.remove("no-scroll");
+  });
+}
+// Burger
+
 // Select
 $(".select").each(function () {
   const _this = $(this),
